4ad82f9726ef8e14e340c551d9111e3177b3c872
zay <64491586+zaytiri@users.noreply.github.com>
2022-11-19 23:39:23 +0000

    Update app.py
    implement main code to schedule multiple jobs from the config file;

--- progscheduler/app.py
+++ progscheduler/app.py
@@ -8,6 +8,7 @@ from progscheduler.arguments import Arguments
 from progscheduler.jobs import open_program
 from scheduler import Scheduler
 
+
 # -e "C:\Users\ziia\AppData\Local\Vivaldi\Application\vivaldi.exe" -a browser -d monday friday saturday
 # -del browser
 
@@ -15,9 +16,20 @@ from scheduler import Scheduler
 def main():
     arguments = Arguments().configure()
 
-    scheduler = Scheduler(lambda: open_program(arguments.executable_path))
+    scheduler = Scheduler()
 
-    scheduler.process(arguments.days_to_schedule, arguments.time_to_schedule)
+    for program in arguments:
+        process_scheduler(scheduler, program)
+
+    scheduler.run()
+
+
+def process_scheduler(scheduler, program_to_schedule):
+    scheduler.set_method_to_schedule(lambda: open_program(program_to_schedule.executable_path.value))
+    scheduler.process(
+        program_to_schedule.days_to_schedule.value,
+        program_to_schedule.time_to_schedule.value
+    )
 
 
 if __name__ == '__main__':
