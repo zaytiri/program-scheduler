861fd01307c248f05b97649f60e4b7de6df65e60
zay <64491586+zaytiri@users.noreply.github.com>
2022-11-20 11:05:51 +0000

    Create program-scheduler.bat

---
+++ program-scheduler.bat
@@ -0,0 +1,5 @@
+@REM progscheduler empty command to run at startup.
+@REM This file needs to be put into C:\Users\<username>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup folder
+@REM to start running when the computer boots up.
+
+progscheduler
