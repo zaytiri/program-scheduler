f70cab513b81a917f8cd1d65d454a2dddd9e78c4
lia <64491586+zaytiri@users.noreply.github.com>
2023-03-04 13:03:02 +0000

    Update progsettings.py

--- progscheduler/utils/progsettings.py
+++ progscheduler/utils/progsettings.py
@@ -3,7 +3,7 @@ import yaml
 
 
 def get_version():
-    path = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '\\progsettings.yaml'
+    path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'progsettings.yaml')
     with open(path, 'r') as settings_file:
         settings = yaml.safe_load(settings_file)['prog'.upper()]
         return settings['version'.upper()]
