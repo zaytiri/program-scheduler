94300449c27fec814833be42de6fdc586dc770e7
zay <64491586+zaytiri@users.noreply.github.com>
2023-09-15 18:26:23 +0100

    change the way to get files due to GUI existing

--- progscheduler/version/progsettings.py
+++ progscheduler/version/progsettings.py
@@ -1,9 +1,15 @@
 import os
+import sys
+
 import yaml
 
 
 def get_version():
-    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'progsettings.yaml')
+    if getattr(sys, 'frozen', False):
+        path = os.path.join(sys._MEIPASS, "files/progsettings.yaml")
+    else:
+        path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'progsettings.yaml')
+
     with open(path, 'r') as settings_file:
         settings = yaml.safe_load(settings_file)['prog'.upper()]
         return settings['version'.upper()]
