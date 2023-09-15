8994301e8673d487383fe4171faa7d5db70fd60f
zay <64491586+zaytiri@users.noreply.github.com>
2023-09-15 18:32:42 +0100

    change settings folder to the home computer location;

--- progscheduler/settings/manager.py
+++ progscheduler/settings/manager.py
@@ -1,6 +1,7 @@
 import os
 
 import argparse
+from os.path import expanduser
 
 from margument.non_repeatable_settings import NonRepeatableSettings
 from margument.options import Options
@@ -10,6 +11,18 @@ from margument.settings_processor import SettingsProcessor
 from progscheduler.settings.generic_arguments import Generic
 from progscheduler.settings.specific_arguments import Specific
 from progscheduler.version.progsettings import get_version
+
+
+def get_path():
+    home = expanduser("~")
+    path = os.path.join(home, 'progscheduler')
+    if not os.path.exists(path):
+        os.mkdir(path)
+    final_path = os.path.join(path, 'settings')
+    if not os.path.exists(final_path):
+        os.mkdir(final_path)
+
+    return final_path
 
 
 class Manager:
@@ -22,13 +35,13 @@ class Manager:
     def configure_arguments(self):
         # manage specific configurations
         specific = Specific()
-        local_settings = RepeatableSettings(path=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'local.yaml'),
+        local_settings = RepeatableSettings(path=os.path.join(get_path(), 'local.yaml'),
                                             program_arguments=specific,
                                             options=Options(show_saved=True, save_main_arg_exists=True))
         specific.are_configs_saved = local_settings.exists()
 
         # manage generic configurations
-        global_settings = NonRepeatableSettings(path=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'global.yaml'),
+        global_settings = NonRepeatableSettings(path=os.path.join(get_path(), 'global.yaml'),
                                                 program_arguments=Generic(),
                                                 options=Options(show_saved=True, save_different=True))
 
